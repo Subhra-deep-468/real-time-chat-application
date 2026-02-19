0fb364a3df8f9ef9918a4b4ccc5f891b13ac9006
subhra-deep <subhradeepchakraborty738@gmail.com>
2026-02-19 20:03:49 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,10 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
